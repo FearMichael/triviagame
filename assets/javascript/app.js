ddfbe969f6cbfd27c95ba48b32d19489c8ea5cbb
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-01-14 17:13:29 -0500

    Functionality and game end

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,33 +3,50 @@ var queryUrl = "https://opentdb.com/api.php?amount=10&category=26&difficulty=med
 var selectedAnswer;
 var triviaQuestionCount = 0;
 var correctAnswer;
-var answerOptions = ["1", "2", "3", "4"];
 var trivia;
 var answers = [];
 var correct = 0;
 var wrong = 0;
+var interval;
+var instance = M.FormSelect.getInstance("option");
+
 // jQuery Selector variables
 var question = $("#question");
-var btn = $(".btn");
+var answerButton = $(".answers");
 var answer1 = $("#answer1");
 var answer2 = $("#answer2");
 var answer3 = $("#answer3");
 var answer4 = $("#answer4");
 var nextQuestion = $("#nextQuestion");
+var timerPlace = $("#timer");
+var checkAnswer = $("#checkAnswer");
+var wrongCount = $("#wrongCount");
+var correctCount = $("#correctCount");
+var wrongAnswerArea = $("#wrongAnswerArea");
+var correctAnswerArea = $("#correctAnswerArea")
+var winPic = $("#winPic");
+var losePic = $("#losePic");
+var winLossHide = $(".winLossHide");
+var triviaOptions = $("#triviaSelect");
+
+//Materialize initialization
+  M.AutoInit();
 
 // Question and answers as JSON
-$.ajax({
+function getInfo() {
+ $.ajax({
     url: queryUrl,
     method: "GET"
   }).then(function(info) {
       trivia = info;
       createTrivia();
-  });
+  });   
+}
 
-//Gen random answer
 
 //Set Question/Answers
 function createTrivia() {
+//Shuffles the answer array randomly
         function shuffle(a) {
             var j, x, i;
             for (i = a.length - 1; i > 0; i--) {
@@ -40,35 +57,186 @@ function createTrivia() {
             };
             return a;
     };
+  var questionNumber = triviaQuestionCount + 1
   correctAnswer = trivia.results[triviaQuestionCount].correct_answer;
   answers = trivia.results[triviaQuestionCount].incorrect_answers;
   answers.push(trivia.results[triviaQuestionCount].correct_answer);
   shuffle(answers);
-  question.html(trivia.results[triviaQuestionCount].question);
+  question.html(questionNumber + ". " + trivia.results[triviaQuestionCount].question);
   answer1.html(answers[0]);
   answer2.html(answers[1]);
   answer3.html(answers[2]);
   answer4.html(answers[3]);
+  timer();
+  console.log(correctAnswer);
+  triviaOptions.hide();
+  winLossHide.show();
 };
   
+//Select Category
+// Gen knowledge: https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple
+//Books https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple
+//Video Games https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple
+// computer science https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple
+// television https://opentdb.com/api.php?amount=10&category=14&difficulty=medium&type=multiple
+
+$("#triviaSelect select").on("change", function(event) {
+    var value = this.value
+    switch(value) {
+        //gen knowledge
+        case "1":
+        queryUrl = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple"
+        break;
+        //books
+        case "2":
+        queryUrl = "https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple"
+        break;
+        //video games
+        case "3":
+        queryUrl = "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple"
+        break;
+        //computer science
+        case "4":
+        queryUrl = "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple"
+        break;
+        //television
+        case "5":
+        queryUrl = "https://opentdb.com/api.php?amount=10&category=14&difficulty=medium&type=multiple"
+        break;
+    }
+    getInfo();
+});
+
+
 // Timer for questions
+function timer() {
+    // var hour = 2;
+    var sec = 30; //30 seconds per question
+    interval = setInterval(function() {
+      timerPlace.text(sec);
+      sec--;
+      if (sec === -1) { //timer will hit 0 by using -1
+        selectedAnswer = "**wrong**"
+          clearInterval(interval);
+          checkInput();
+      };
+    //   setInterval();
+    //   if (sec == 00) {
+    //     hour--;
+    //     sec = 60;
+    //     if (hour == 0) {
+    //       hour = 2;
+    //     }
+    //   }
+    },1000);
+  };
 
+  //Game end function
+  function gameEnd() {
+      if (triviaQuestionCount === 9) {
+        triviaOptions.show();
+        winLossHide.hide();
+        clearInterval(interval);
+        question.html("Want to play again? Select your next category!")
+      }
+  }
 
+  //Show answer and stop taking input
+// function showAnswer() {
+//     if (answer1.text() ==  correctAnswer) {
+//             answer1.animate({textColor: "red"}, 1000 * 5);
+//     } 
+//     else if (answer2.text() == correctAnswer) {
+//             answer2.animate({textColor: "red"}, 1000 * 5);
+//     } 
+//     else if (answer3.text() == correctAnswer) {
+//             answer3.animate({textColor: "red"}, 1000 * 5);
+//     } 
+//     else if (answer4.text() == correctAnswer) {
+//             answer4.animate({textColor: "red"}, 1000 * 5);
 
-//Next question
-nextQuestion.on("click", function(event){
-    nextQuestion.hide();
-    winPic.hide();
+//     }
+// };
+// Show pic if answer is correct
+function showCorrectPic() {
+    winLossHide.hide();
+    winPic.show();
+    setTimeout(function() {
+        winLossHide.show();
+        winPic.hide();
+        nextQuestionFunc();
+    }, 1000 * 4)
+};
+
+// show pic if answer is wrong
+function showWrongPic() {
+    winLossHide.hide();
+    losePic.show();
+    setTimeout(function() {
+        winLossHide.show();
+        losePic.hide();
+        nextQuestionFunc();
+    }, 1000 * 4)
+};
+
+  //Answer Right or wrong
+  function checkInput() {
+      if (selectedAnswer == "") {
+
+      }
+      else if (selectedAnswer == correctAnswer)
+      {
+          correct ++;
+          correctAnswerArea.append("<p>" + trivia.results[triviaQuestionCount].question + "<br/>" + trivia.results[triviaQuestionCount].correct_answer);
+          correctCount.text(correct);
+          nextQuestion.show();
+          triviaQuestionCount++;
+          showCorrectPic();
+          clearInterval(interval);
+
+      } 
+      else if (selectedAnswer != correctAnswer) 
+      {
+          wrong++;
+          wrongAnswerArea.append("<p>" + trivia.results[triviaQuestionCount].question + "<br/>" + trivia.results[triviaQuestionCount].correct_answer);
+          wrongCount.text(wrong);
+          nextQuestion.show();
+          triviaQuestionCount++;
+          showWrongPic();
+          clearInterval(interval);
+      }
+  };
+//Next question function
+function nextQuestionFunc() {
+    clearInterval(interval);
     createTrivia();
-})
+    selectedAnswer = "";
+    nextQuestion.hide();
+    winLossHide.show();
+    winPic.hide();
+    losePic.hide()
+    gameEnd();
+};
+//Next question
+// nextQuestion.on("click", function(event){
+//     nextQuestionFunc();
+// });
+//Record the answer input
+answerButton.on("click", function(event) {
+    selectedAnswer = $(this).text();
+});
 
 // Logic to figure out if answer is right or wrong
-btn.on("click", function(event) {
-    console.log(event);
-    selectedAnswer = $("this").text();
-    if (selectedAnswer == correctAnswer) {
-        nextQuestion.show();
-        winPic.show();
-    };
-    triviaQuestionCount++;
+checkAnswer.on("click", function(event) {
+    if (selectedAnswer === "") {
+//Do nothing if no answer is selected
+    } else {
+    checkAnswer.attr("disabled", "true");
+    checkInput();
+    setTimeout(function() {
+        checkAnswer.removeAttr("disabled"); //Prevents from repeated clicks and false "wrongs"
+    }, 1000 * 4);
+    }
+    
+
 });
